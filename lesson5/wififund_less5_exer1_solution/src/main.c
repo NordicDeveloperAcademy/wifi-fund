acee351782fde888b09b221cb7ad9d143fd07c8c
Sollie <heidi-irene.sollie@nordicsemi.no>
2023-06-26 11:43:05 +0200

    Lesson 5 Exercise 1 - server address added

--- lesson5/wififund_less5_exer1_solution/src/main.c
+++ lesson5/wififund_less5_exer1_solution/src/main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <string.h>
 #include <zephyr/logging/log.h>
 #include <dk_buttons_and_leds.h>
 
@@ -19,9 +20,11 @@ LOG_MODULE_REGISTER(Lesson5_Exercise1, LOG_LEVEL_INF);
 K_SEM_DEFINE(wifi_connected_sem, 0, 1);
 
 #define HTTP_PORT 80
-#define HTTP_HOSTNAME "google.com"
+#define HTTP_HOSTNAME "d1jglomgqgmujc.cloudfront.net"
 
 #define RECV_BUF_SIZE 2048
+
+#define CONFIG_USE_IPV6 true
 
 static int sock;
 static struct sockaddr_storage server;
@@ -78,7 +81,7 @@ static int wifi_connect() {
 	LOG_INF("Connecting to Wi-Fi");
 	int err = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &cnx_params, sizeof(struct wifi_connect_req_params));
 	if (err) {
-		LOG_ERR("Connecting to Wi-Fi failed. Error: %d", err);
+		LOG_ERR("Connecting to Wi-Fi failed, err: %d", err);
 		return ENOEXEC;
 	}
 	k_sem_take(&wifi_connected_sem, K_FOREVER);
@@ -98,7 +101,7 @@ static int server_resolve(void)
 
 	err = getaddrinfo(HTTP_HOSTNAME, STRINGIFY(HTTP_PORT), &hints, &result);
 	if (err != 0) {
-		LOG_INF("getaddrinfo failed, err: %d", err);
+		LOG_INF("getaddrinfo failed, err: %d, %s", err, gai_strerror(err));
 		return -EIO;
 	}
 
@@ -106,15 +109,16 @@ static int server_resolve(void)
 		LOG_INF("Error, address not found");
 		return -ENOENT;
 	}
+	
 	struct sockaddr_in *server4 = ((struct sockaddr_in *)&server);
-	server4->sin_addr.s_addr =
+	server4->sin_addr.s_addr = 
 		((struct sockaddr_in *)result->ai_addr)->sin_addr.s_addr;
 	server4->sin_family = AF_INET;
 	server4->sin_port = ((struct sockaddr_in *)result->ai_addr)->sin_port;
 
 	char ipv4_addr[NET_IPV4_ADDR_LEN];
 	inet_ntop(AF_INET, &server4->sin_addr.s_addr, ipv4_addr,
-		  sizeof(ipv4_addr));
+		sizeof(ipv4_addr));
 	LOG_INF("IPv4 Address found %s", ipv4_addr);
 
 	freeaddrinfo(result);
@@ -127,15 +131,16 @@ static int server_connect(void)
 	int err;
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock < 0) {
-		LOG_ERR("Failed to set up HTTP socket, err: %d.", errno);
+		LOG_INF("Failed to set up HTTP socket, err: %d, %s", errno, strerror(errno));
 		return -errno;
 	}
 
 	err = connect(sock, (struct sockaddr *)&server,
 		      sizeof(struct sockaddr_in));
 	if (err < 0) {
-		LOG_ERR("Connect failed : %d", errno);
+		LOG_INF("Connecting to server failed, err: %d, %s", errno, strerror(errno));
 		return -errno;
+
 	}
 	LOG_INF("Successfully connected to server");
 
@@ -155,7 +160,27 @@ static void response_cb(struct http_response *rsp,
     LOG_INF("Response status %s", rsp->http_status);
 }
 
-static int run_queries(void)
+static int client_post_send(void)
+{
+	int ret = 0;
+	struct http_request req;
+
+	memset(&req, 0, sizeof(req));
+
+	req.method = HTTP_POST;
+	req.url = "/";
+	req.host = HTTP_HOSTNAME;
+	req.protocol = "HTTP/1.1";
+	req.response = response_cb;
+	req.recv_buf = recv_buf;
+	req.recv_buf_len = sizeof(recv_buf);
+
+	ret = http_client_req(sock, &req, 5000, "IPv4 POST");
+
+	return ret;
+}
+
+static int client_get_send(void)
 {
 	int ret = 0;
 	struct http_request req;
@@ -175,17 +200,30 @@ static int run_queries(void)
 	return ret;
 }
 
+static void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	/* STEP 10 - Send a GET request or PUT request upon button triggers */
+	if (has_changed & DK_BTN1_MSK && button_state & DK_BTN1_MSK) {
+		client_get_send();
+	} else if (has_changed & DK_BTN2_MSK && button_state & DK_BTN2_MSK) {
+		client_post_send();
+	}
+}
+
 int main(void)
 {
-	int ret = 0;
+	
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LED library");
+	}
+	
 	if (wifi_connect() != 0) {
 		LOG_ERR("Failed to connect to Wi-Fi");
 	}
 
-	if (dk_leds_init() != 0) {
-		LOG_ERR("Failed to initialize the LED library");
+	if (dk_buttons_init(button_handler) != 0) {
+		LOG_ERR("Failed to initialize the buttons library");
 	}
-
 	if (server_resolve() != 0) {
 		LOG_INF("Failed to resolve server name");
 		return 0;
@@ -196,11 +234,9 @@ int main(void)
 		return 0;
 	}
 
-	while (true) {
-		ret = run_queries();
-		if (ret < 0) {
-			break;
-		}
+	while (1) {
+		k_sleep(K_FOREVER);
+
 	}
 
 	close(sock);
