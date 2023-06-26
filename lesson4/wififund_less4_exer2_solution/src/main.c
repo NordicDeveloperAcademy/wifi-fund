9873de81a80b91febaa50c450321d3df8c81ab1a
Sollie <heidi-irene.sollie@nordicsemi.no>
2023-06-26 17:06:44 +0200

    TLS not implemented yet

---
+++ lesson4/wififund_less4_exer2_solution/src/main.c
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/net/wifi.h>
+#include <zephyr/net/wifi_mgmt.h>
+
+#include <zephyr/net/socket.h>
+#include <zephyr/net/mqtt.h>
+#include <dk_buttons_and_leds.h>
+
+#include "mqtt_connection.h"
+
+LOG_MODULE_REGISTER(Lesson4_Exercise2, LOG_LEVEL_INF);
+K_SEM_DEFINE(wifi_connected_sem, 0, 1);
+
+static struct mqtt_client client;
+static struct pollfd fds;
+static struct net_mgmt_event_callback wifi_connect_cb;
+
+static int wifi_args_to_params(struct wifi_connect_req_params *params)
+{
+
+	params->ssid = CONFIG_WIFI_CREDENTIALS_STATIC_SSID;
+	params->ssid_length = strlen(params->ssid);
+	params->psk = CONFIG_WIFI_CREDENTIALS_STATIC_PASSWORD;
+	params->psk_length = strlen(params->psk);
+	params->channel = WIFI_CHANNEL_ANY;
+	params->security = 1;
+	params->mfp = WIFI_MFP_OPTIONAL;
+	params->timeout = SYS_FOREVER_MS;
+
+	return 0;
+}
+
+static void wifi_connect_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		LOG_INF("Connected to Wi-Fi Network: %s", CONFIG_WIFI_CREDENTIALS_STATIC_SSID);
+        dk_set_led_on(DK_LED1);
+		k_sem_give(&wifi_connected_sem);
+		break;
+	default:
+		break;
+	}
+}
+
+static int wifi_connect() 
+{
+	struct wifi_connect_req_params cnx_params;
+	struct net_if *iface = net_if_get_default();
+	if (iface == NULL) {
+		LOG_ERR("Returned network interface is NULL");
+		return -1;
+	}
+
+	/* Sleep to allow initialization of Wi-Fi driver */
+	k_sleep(K_SECONDS(1));
+
+	net_mgmt_init_event_callback(&wifi_connect_cb, wifi_connect_handler, NET_EVENT_WIFI_CONNECT_RESULT);
+	net_mgmt_add_event_callback(&wifi_connect_cb);
+
+	wifi_args_to_params(&cnx_params);
+
+	LOG_INF("Connecting to Wi-Fi");
+	int err = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &cnx_params, sizeof(struct wifi_connect_req_params));
+	if (err) {
+		LOG_ERR("Connecting to Wi-Fi failed, err: %d", err);
+		return ENOEXEC;
+	}
+	k_sem_take(&wifi_connected_sem, K_FOREVER);
+	k_sleep(K_SECONDS(6));
+
+	return 0;
+}
+
+static void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & DK_BTN1_MSK && button_state & DK_BTN1_MSK) {
+	/* STEP 7.2 - When button 1 is pressed, call data_publish() to publish a message */	
+		int err = data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
+				CONFIG_BUTTON1_EVENT_PUBLISH_MSG, sizeof(CONFIG_BUTTON1_EVENT_PUBLISH_MSG)-1);
+		if (err) {
+			LOG_ERR("Failed to send message, %d", err);
+			return;	
+		}
+	break;
+	} else if (has_changed & DK_BTN2_MSK && button_state & DK_BTN2_MSK) {
+		int err = data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
+				CONFIG_BUTTON2_EVENT_PUBLISH_MSG, sizeof(CONFIG_BUTTON2_EVENT_PUBLISH_MSG)-1);
+		if (err) {
+			LOG_ERR("Failed to send message, %d", err);
+			return;	
+		}
+	}
+	break;
+}
+
+static void connect_mqtt(void)
+{
+	int err;
+	uint32_t connect_attempt = 0;
+
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LED library");
+	}
+
+	if (dk_buttons_init(button_handler) != 0) {
+		LOG_ERR("Failed to initialize the buttons library");
+	}
+
+	err = client_init(&client);
+	if (err) {
+		LOG_ERR("Failed to initialize MQTT client: %d", err);
+		return;
+	}
+
+do_connect:
+	if (connect_attempt++ > 0) {
+		LOG_INF("Reconnecting in %d seconds...",
+			CONFIG_MQTT_RECONNECT_DELAY_S);
+		k_sleep(K_SECONDS(CONFIG_MQTT_RECONNECT_DELAY_S));
+	}
+	err = mqtt_connect(&client);
+	if (err) {
+		LOG_ERR("Error in mqtt_connect: %d", err);
+		goto do_connect;
+	}
+
+	err = fds_init(&client,&fds);
+	if (err) {
+		LOG_ERR("Error in fds_init: %d", err);
+		return;
+	}
+
+	while (1) {
+		err = poll(&fds, 1, mqtt_keepalive_time_left(&client));
+		if (err < 0) {
+			LOG_ERR("Error in poll(): %d", errno);
+			break;
+		}
+
+		err = mqtt_live(&client);
+		if ((err != 0) && (err != -EAGAIN)) {
+			LOG_ERR("Error in mqtt_live: %d", err);
+			break;
+		}
+
+		if ((fds.revents & POLLIN) == POLLIN) {
+			err = mqtt_input(&client);
+			if (err != 0) {
+				LOG_ERR("Error in mqtt_input: %d", err);
+				break;
+			}
+		}
+
+		if ((fds.revents & POLLERR) == POLLERR) {
+			LOG_ERR("POLLERR");
+			break;
+		}
+
+		if ((fds.revents & POLLNVAL) == POLLNVAL) {
+			LOG_ERR("POLLNVAL");
+			break;
+		}
+	}
+
+	LOG_INF("Disconnecting MQTT client");
+
+	err = mqtt_disconnect(&client);
+	if (err) {
+		LOG_ERR("Could not disconnect MQTT client: %d", err);
+	}
+	goto do_connect;
+}
+
+void main(void)
+{
+	if (dk_leds_init() != 0) {
+		LOG_ERR("Failed to initialize the LED library");
+	}
+	
+	if (wifi_connect() != 0) {
+		LOG_ERR("Failed to connect to Wi-Fi");
+	}
+
+	if (dk_buttons_init(button_handler) != 0) {
+		LOG_ERR("Failed to initialize the buttons library");
+	}
+	
+	LOG_INF("Connecting to MQTT Broker...");
+
+	connect_mqtt();
+
+	return 0;
+}
