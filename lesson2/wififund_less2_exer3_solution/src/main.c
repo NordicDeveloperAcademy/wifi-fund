f62e493f63ec2665147fe94ae0beb4843fd86d84
Sollie <heidi-irene.sollie@nordicsemi.no>
2023-06-26 16:24:11 +0200

    Update

---
+++ lesson2/wififund_less2_exer3_solution/src/main.c
@@ -0,0 +1,375 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/kernel.h>
+
+#include <dk_buttons_and_leds.h>
+
+#include <zephyr/net/wifi.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <net/wifi_credentials.h>
+
+/* STEP 2 - Include the header files for Bluetooth LE */
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <bluetooth/services/wifi_provisioning.h>
+
+
+LOG_MODULE_REGISTER(Lesson2_Exercise3, LOG_LEVEL_INF);
+K_SEM_DEFINE(wifi_connected_sem, 0, 1);
+
+#define ADV_DATA_UPDATE_INTERVAL      5
+
+#define ADV_PARAM_UPDATE_DELAY        1
+
+#define ADV_DATA_VERSION_IDX          (BT_UUID_SIZE_128 + 0)
+#define ADV_DATA_FLAG_IDX             (BT_UUID_SIZE_128 + 1)
+#define ADV_DATA_FLAG_PROV_STATUS_BIT BIT(0)
+#define ADV_DATA_FLAG_CONN_STATUS_BIT BIT(1)
+#define ADV_DATA_RSSI_IDX             (BT_UUID_SIZE_128 + 3)
+
+#define PROV_BT_LE_ADV_PARAM_FAST BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE, \
+						BT_GAP_ADV_FAST_INT_MIN_2, \
+						BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+
+#define PROV_BT_LE_ADV_PARAM_SLOW BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE, \
+						BT_GAP_ADV_SLOW_INT_MIN, \
+						BT_GAP_ADV_SLOW_INT_MAX, NULL)
+
+#define ADV_DAEMON_STACK_SIZE 4096
+#define ADV_DAEMON_PRIORITY 5
+
+K_THREAD_STACK_DEFINE(adv_daemon_stack_area, ADV_DAEMON_STACK_SIZE);
+static struct k_work_q adv_daemon_work_q;
+
+static uint8_t device_name[] = {'P', 'V', '0', '0', '0', '0', '0', '0'};
+static uint8_t prov_svc_data[] = {BT_UUID_PROV_VAL, 0x00, 0x00, 0x00, 0x00};
+
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_PROV_VAL),
+	BT_DATA(BT_DATA_NAME_COMPLETE, device_name, sizeof(device_name)),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA(BT_DATA_SVC_DATA128, prov_svc_data, sizeof(prov_svc_data)),
+};
+
+static struct k_work_delayable update_adv_param_work;
+static struct k_work_delayable update_adv_data_work;
+
+static struct net_mgmt_event_callback wifi_prov_cb;
+
+static void wifi_connect_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		LOG_INF("Connected to Wi-Fi Network");
+        dk_set_led_on(DK_LED1);
+		k_sem_give(&wifi_connected_sem);
+		break;
+	default:
+		break;
+	}
+}
+
+static void wifi_register_cb(void)
+{
+	LOG_INF("Registering Wi-Fi events");
+	net_mgmt_init_event_callback(&wifi_prov_cb,
+				     wifi_connect_handler,
+				     NET_EVENT_WIFI_CONNECT_RESULT);
+	net_mgmt_add_event_callback(&wifi_prov_cb);
+	
+}
+
+static void update_wifi_status_in_adv(void)
+{
+
+
+	prov_svc_data[ADV_DATA_VERSION_IDX] = PROV_SVC_VER;
+
+	if (!bt_wifi_prov_state_get()) {
+		prov_svc_data[ADV_DATA_FLAG_IDX] &= ~ADV_DATA_FLAG_PROV_STATUS_BIT;
+	} else {
+		prov_svc_data[ADV_DATA_FLAG_IDX] |= ADV_DATA_FLAG_PROV_STATUS_BIT;
+	}
+
+	struct net_if *iface = net_if_get_default();
+	struct wifi_iface_status status = { 0 };
+
+	int err = net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,
+				sizeof(struct wifi_iface_status));
+	if ((err != 0) || (status.state < WIFI_STATE_ASSOCIATED)) {
+		prov_svc_data[ADV_DATA_FLAG_IDX] &= ~ADV_DATA_FLAG_CONN_STATUS_BIT;
+		prov_svc_data[ADV_DATA_RSSI_IDX] = INT8_MIN;
+	} else {
+		prov_svc_data[ADV_DATA_FLAG_IDX] |= ADV_DATA_FLAG_CONN_STATUS_BIT;
+		prov_svc_data[ADV_DATA_RSSI_IDX] = status.rssi;
+	}
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	if (err) {
+		LOG_ERR("BT Connection failed (err 0x%02x).\n", err);
+		return;
+	}
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	LOG_INF("BT Connected: %s", addr);
+
+	k_work_cancel_delayable(&update_adv_data_work);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	LOG_INF("BT Disconnected: %s (reason 0x%02x).\n", addr, reason);
+
+	k_work_reschedule_for_queue(&adv_daemon_work_q, &update_adv_param_work,
+				K_SECONDS(ADV_PARAM_UPDATE_DELAY));
+	k_work_reschedule_for_queue(&adv_daemon_work_q, &update_adv_data_work, K_NO_WAIT);
+}
+
+static void identity_resolved(struct bt_conn *conn, const bt_addr_le_t *rpa,
+				const bt_addr_le_t *identity)
+{
+	char addr_identity[BT_ADDR_LE_STR_LEN];
+	char addr_rpa[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(identity, addr_identity, sizeof(addr_identity));
+	bt_addr_le_to_str(rpa, addr_rpa, sizeof(addr_rpa));
+
+	LOG_INF("BT Identity resolved %s -> %s.\n", addr_rpa, addr_identity);
+}
+
+static void security_changed(struct bt_conn *conn, bt_security_t level,
+				enum bt_security_err err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (!err) {
+		LOG_INF("BT Security changed: %s level %u.\n", addr, level);
+	} else {
+		LOG_ERR("BT Security failed: %s level %u err %d.\n", addr, level,
+			   err);
+	}
+}
+
+static struct bt_conn_cb conn_callbacks = {
+	.connected = connected,
+	.disconnected = disconnected,
+	.identity_resolved = identity_resolved,
+	.security_changed = security_changed,
+};
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("BT Pairing cancelled: %s.\n", addr);
+}
+
+static struct bt_conn_auth_cb conn_auth_callbacks = {
+	.cancel = auth_cancel,
+};
+
+static void pairing_complete(struct bt_conn *conn, bool bonded)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	LOG_INF("BT pairing completed: %s, bonded: %d\n", addr, bonded);
+}
+
+static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
+{
+	LOG_INF("BT Pairing Failed (%d). Disconnecting.\n", reason);
+	bt_conn_disconnect(conn, BT_HCI_ERR_AUTH_FAIL);
+}
+
+static struct bt_conn_auth_info_cb auth_info_cb_display = {
+
+	.pairing_complete = pairing_complete,
+	.pairing_failed = pairing_failed,
+};
+
+static void update_adv_data_task(struct k_work *item)
+{
+	int err;
+
+	update_wifi_status_in_adv();
+	err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err != 0) {
+		LOG_INF("Cannot update advertisement data, err = %d\n", err);
+	}
+	k_work_reschedule_for_queue(&adv_daemon_work_q, &update_adv_data_work,
+				K_SECONDS(ADV_DATA_UPDATE_INTERVAL));
+}
+
+static void update_adv_param_task(struct k_work *item)
+{
+	int err;
+
+	err = bt_le_adv_stop();
+	if (err != 0) {
+		LOG_ERR("Cannot stop advertisement: err = %d\n", err);
+		return;
+	}
+
+	err = bt_le_adv_start(prov_svc_data[ADV_DATA_FLAG_IDX] & ADV_DATA_FLAG_PROV_STATUS_BIT ?
+		PROV_BT_LE_ADV_PARAM_SLOW : PROV_BT_LE_ADV_PARAM_FAST,
+		ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err != 0) {
+		LOG_ERR("Cannot start advertisement: err = %d\n", err);
+	}
+}
+
+static void byte_to_hex(char *ptr, uint8_t byte, char base)
+{
+	int i, val;
+
+	for (i = 0, val = (byte & 0xf0) >> 4; i < 2; i++, val = byte & 0x0f) {
+		if (val < 10) {
+			*ptr++ = (char) (val + '0');
+		} else {
+			*ptr++ = (char) (val - 10 + base);
+		}
+	}
+}
+
+static void update_dev_name(struct net_linkaddr *mac_addr)
+{
+	byte_to_hex(&device_name[2], mac_addr->addr[3], 'A');
+	byte_to_hex(&device_name[4], mac_addr->addr[4], 'A');
+	byte_to_hex(&device_name[6], mac_addr->addr[5], 'A');
+}
+
+static void get_wifi_credential(void *cb_arg, const char *ssid, size_t ssid_len)
+{
+	struct wifi_credentials_personal config;
+
+	wifi_credentials_get_by_ssid_personal_struct(ssid, ssid_len, &config);
+	memcpy((struct wifi_credentials_personal *)cb_arg, &config, sizeof(config));
+}
+
+int main(void)
+{
+	int err;
+
+	/* Sleep 1 seconds to allow initialization of wifi driver. */
+	k_sleep(K_SECONDS(1));
+
+	bt_conn_cb_register(&conn_callbacks);
+	bt_conn_auth_cb_register(&conn_auth_callbacks);
+	bt_conn_auth_info_cb_register(&auth_info_cb_display);
+
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d).\n", err);
+		return 0;
+	}
+
+	LOG_INF("Bluetooth initialized.\n");
+	err = bt_wifi_prov_init();
+	if (err == 0) {
+		LOG_INF("Wi-Fi provisioning service starts successfully.\n");
+	} else {
+		LOG_ERR("Error occurs when initializing Wi-Fi provisioning service.\n");
+		return 0;
+	}
+
+	/* Prepare advertisement data */
+	struct net_linkaddr *mac_addr = net_if_get_link_addr(iface);
+	char device_name_str[sizeof(device_name) + 1];
+
+	if (mac_addr) {
+		update_dev_name(mac_addr);
+	}
+	device_name_str[sizeof(device_name_str) - 1] = '\0';
+	memcpy(device_name_str, device_name, sizeof(device_name));
+	bt_set_name(device_name_str);
+
+	err = bt_le_adv_start(prov_svc_data[ADV_DATA_FLAG_IDX] & ADV_DATA_FLAG_PROV_STATUS_BIT ?
+		PROV_BT_LE_ADV_PARAM_SLOW : PROV_BT_LE_ADV_PARAM_FAST,
+		ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("BT Advertising failed to start (err %d)\n", err);
+		return 0;
+	}
+	LOG_INF("BT Advertising successfully started.\n");
+
+	update_wifi_status_in_adv();
+
+	k_work_queue_init(&adv_daemon_work_q);
+	k_work_queue_start(&adv_daemon_work_q, adv_daemon_stack_area,
+			K_THREAD_STACK_SIZEOF(adv_daemon_stack_area), ADV_DAEMON_PRIORITY,
+			NULL);
+
+	k_work_init_delayable(&update_adv_param_work, update_adv_param_task);
+	k_work_init_delayable(&update_adv_data_work, update_adv_data_task);
+	k_work_schedule_for_queue(&adv_daemon_work_q, &update_adv_data_work,
+				K_SECONDS(ADV_DATA_UPDATE_INTERVAL));
+
+	/* Search for stored wifi credential and apply */
+	struct wifi_credentials_personal config = { 0 };
+	struct net_if *iface = net_if_get_default();
+	struct wifi_connect_req_params cnx_params = { 0 };
+	
+	wifi_credentials_for_each_ssid(get_wifi_credential, &config);
+
+	if (config.header.ssid_len > 0) {
+		LOG_INF("Configuration found. Try to apply.\n");
+
+		cnx_params.ssid = config.header.ssid;
+		cnx_params.ssid_length = config.header.ssid_len;
+		cnx_params.security = config.header.type;
+
+		cnx_params.psk = NULL;
+		cnx_params.psk_length = 0;
+		cnx_params.sae_password = NULL;
+		cnx_params.sae_password_length = 0;
+
+		if (config.header.type != WIFI_SECURITY_TYPE_NONE) {
+			cnx_params.psk = config.password;
+			cnx_params.psk_length = config.password_len;
+		}
+
+		cnx_params.channel = WIFI_CHANNEL_ANY;
+		cnx_params.band = config.header.flags & WIFI_CREDENTIALS_FLAG_5GHz ?
+				WIFI_FREQ_BAND_5_GHZ : WIFI_FREQ_BAND_2_4_GHZ;
+		cnx_params.mfp = WIFI_MFP_OPTIONAL;
+		err = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+			&cnx_params, sizeof(struct wifi_connect_req_params));
+		if (err < 0) {
+			LOG_ERR("Cannot apply saved Wi-Fi configuration, err = %d.\n", err);
+		} else {
+			LOG_INF("Configuration applied.\n");
+		}
+	}
+	wifi_register_cb(void);
+	k_sem_take(&wifi_connected_sem, K_FOREVER);
+}
