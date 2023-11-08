635191d202e859d10a8ed6604897516aee32c3dc
Marte <marte.myrvold@nordicsemi.no>
2023-11-08 15:03:18 +0100

    Finished code for less6_exer2

--- lesson6/wififund_less6_exer2_solution/src/main.c
+++ lesson6/wififund_less6_exer2_solution/src/main.c
@@ -10,16 +10,13 @@
 #include <dk_buttons_and_leds.h>
 
 #include <zephyr/net/net_if.h>
-
 #include <zephyr/net/net_event.h>
 #include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
 #include <net/wifi_mgmt_ext.h>
 
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/socket.h>
-
-#include <zephyr/net/http/client.h>
-
 
 LOG_MODULE_REGISTER(Lesson6_Exercise2, LOG_LEVEL_INF);
 
@@ -33,6 +30,32 @@ K_SEM_DEFINE(ipv4_obtained_sem, 0, 1);
 
 #define IPV4_MGMT_EVENTS (NET_EVENT_IPV4_ADDR_ADD | \
 				NET_EVENT_IPV4_ADDR_DEL)
+
+/* STEP x - Define the port and IPv4 address for the echo server */
+#define SERVER_PORT 7777
+#define SERVER_IPV4_ADDR "192.168.32.119"
+
+#define SSTRLEN(s) (sizeof(s) - 1)
+#define RECV_BUF_SIZE 256
+
+/* STEP x - Define counter and   */
+static int counter = 0;
+static int recv_counter = 0;
+
+/* STEP 5.1 - Declare the structure for the socket and server address */
+static int sock;
+static struct sockaddr_in server;
+
+/* STEP 5.2 - Declare the buffer for receiving from server */
+static uint8_t recv_buf[RECV_BUF_SIZE];
+
+/* STEP x - */
+int send_packet();
+int receive_packet();
+
+/* STEP x - Define wakeup time and interval for TWT (interval = wake up time + sleeping time)  */
+#define TWT_WAKE_INTERVAL_MS 65
+#define TWT_INTERVAL_S 		 30
 
 bool nrf_wifi_twt_enabled = 0;
 static uint32_t twt_flow_id = 1;
@@ -56,6 +79,7 @@ static int wifi_args_to_params(struct wifi_connect_req_params *params)
 
 static void handle_wifi_twt_event(struct net_mgmt_event_callback *cb)
 {
+	/* STEP x - */
 	const struct wifi_twt_params *resp = (const struct wifi_twt_params *)cb->info;
 	
 	if (resp->operation == WIFI_TWT_TEARDOWN) {
@@ -67,16 +91,14 @@ static void handle_wifi_twt_event(struct net_mgmt_event_callback *cb)
 
 	twt_flow_id = resp->flow_id;
 
-
+	/* STEP x - */
 	if (resp->resp_status == WIFI_TWT_RESP_RECEIVED) {
 		LOG_INF("TWT response: %s",
 		      wifi_twt_setup_cmd_txt(resp->setup_cmd));
 
 		if (resp->setup_cmd == WIFI_TWT_SETUP_CMD_ACCEPT) {
 			nrf_wifi_twt_enabled = 1;
-		}
 		
-
 		LOG_INF("== TWT negotiated parameters ==");
 		LOG_INF("TWT Dialog token: %d",
 		      resp->dialog_token);
@@ -92,11 +114,14 @@ static void handle_wifi_twt_event(struct net_mgmt_event_callback *cb)
 		      resp->setup.announce ? "true" : "false");
 		LOG_INF("TWT trigger: %s",
 		      resp->setup.trigger ? "true" : "false");
-		LOG_INF("TWT wake interval: %d us",
-		      resp->setup.twt_wake_interval);
-		LOG_INF("TWT interval: %lld us",
+		LOG_INF("TWT wake interval: %d ms (%d us)",
+		      resp->setup.twt_wake_interval/USEC_PER_MSEC,
+			  resp->setup.twt_wake_interval);
+		LOG_INF("TWT interval: %lld s (%lld us)",
+			  resp->setup.twt_interval/USEC_PER_SEC,
 		      resp->setup.twt_interval);
-		LOG_INF("========================");
+		LOG_INF("===============================");
+		}
 	} 
 	else {
 		LOG_INF("TWT response timed out\n");
@@ -116,13 +141,19 @@ static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
 		LOG_INF("Disconnected from Wi-Fi Network");
         dk_set_led_off(DK_LED1);
 		break;
+	/* STEP x - */
 	case NET_EVENT_WIFI_TWT:
 		handle_wifi_twt_event(cb);
 		break;
+	/* STEP x - */
 	case NET_EVENT_WIFI_TWT_SLEEP_STATE:
 		int *twt_state;
 		twt_state = (int *)(cb->info);
 		LOG_INF("TWT sleep state: %s", *twt_state ? "awake" : "sleeping" );
+		if (*twt_state == WIFI_TWT_STATE_AWAKE) {
+			send_packet();
+				receive_packet();
+		}
 		break;
 	default:
         LOG_ERR("Unknown event: %d", mgmt_event);
@@ -181,6 +212,7 @@ static int wifi_connect() {
 
 int wifi_set_twt()
 {
+	/* STEP x - */
 	struct net_if *iface = net_if_get_default();
 
 	struct wifi_twt_params params = { 0 };
@@ -190,22 +222,26 @@ int wifi_set_twt()
 	params.flow_id = twt_flow_id;
 	params.dialog_token = 1;
 
+
 	if (!nrf_wifi_twt_enabled){
+		/* STEP x - */
 		params.operation = WIFI_TWT_SETUP;
 		params.setup.responder = 0;
 		params.setup.trigger = 1;
 		params.setup.implicit = 1;
 		params.setup.announce = 1;
-		params.setup.twt_wake_interval = 65000;
-		params.setup.twt_interval = 5240000;
+		params.setup.twt_wake_interval = TWT_WAKE_INTERVAL_MS * USEC_PER_MSEC;
+		params.setup.twt_interval = TWT_INTERVAL_S * USEC_PER_SEC;
 	}
 	else {
+		/* STEP x - */
 		params.operation = WIFI_TWT_TEARDOWN;
 		params.teardown.teardown_all = 1;
 		twt_flow_id = twt_flow_id<WIFI_MAX_TWT_FLOWS ? twt_flow_id+1 : 1;
 		nrf_wifi_twt_enabled = 0;
 	}
 
+	/* STEP x - */
 	if (net_mgmt(NET_REQUEST_WIFI_TWT, iface, &params, sizeof(params))) {
 		LOG_ERR("%s with %s failed, reason : %s",
 			wifi_twt_operation_txt(params.operation),
@@ -213,12 +249,81 @@ int wifi_set_twt()
 			wifi_twt_get_err_code_str(params.fail_reason));
 		return -1;
 	}
-	LOG_INF("TWT operation %s with flow_id: %d requested", 
-			wifi_twt_operation_txt(params.operation),
-			params.flow_id);
+	LOG_INF("TWT operation %s requested", 
+			wifi_twt_operation_txt(params.operation));
 	return 0;
 }
 
+static int server_connect(void)
+{
+	int err;
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(SERVER_PORT);
+
+	err = inet_pton(AF_INET, SERVER_IPV4_ADDR, &server.sin_addr);
+	if (err <= 0) {
+		LOG_ERR("Invalid address, err: %d, %s", errno, strerror(errno));
+		close(sock);
+		return -errno;
+	}
+
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (sock < 0) {
+		LOG_INF("Failed to create socket, err: %d, %s", errno, strerror(errno));
+		return -errno;
+	}
+
+	err = connect(sock, (struct sockaddr *)&server, sizeof(server));
+	if (err < 0) {
+		LOG_INF("Connecting to server failed, err: %d, %s", errno, strerror(errno));
+		return -errno;
+	}
+	LOG_INF("Connected to server");
+
+	return 0;
+}
+
+int send_packet()
+{
+	int err;
+
+	int bytes_written;
+	char buffer[30] = {"\0"};
+	bytes_written = snprintf(buffer, 30, "Hello from nRF70 Series! %d", counter);
+	if (bytes_written < 0){
+		LOG_INF("Unable to write to buffer, err: %d", bytes_written);
+		return bytes_written;
+	}
+	
+	err = sendto(sock, &buffer, SSTRLEN(buffer), 0, (struct sockaddr *)&server, sizeof(server));
+	if (err < 0) {
+		LOG_ERR("Failed to send message, err: %d, %s", errno, strerror(errno));
+		return -errno;
+	}
+	LOG_INF("Successfully sent message: %s", buffer);
+	counter++;
+	return 0;
+}
+
+int receive_packet()
+{
+	int received;
+	socklen_t addr_len;
+	addr_len = sizeof(server);
+	received = recvfrom(sock, recv_buf, sizeof(recv_buf) - 1, 0, (struct sockaddr *)&server, &addr_len);
+
+	if (received <= 0) {
+		return -1;
+	}
+
+	recv_buf[received] = 0;
+	LOG_INF("Data received from the server: (%s)", recv_buf);
+
+	recv_counter++;
+	return 0;
+}
 
 static void button_handler(uint32_t button_state, uint32_t has_changed)
 {
@@ -229,6 +334,7 @@ static void button_handler(uint32_t button_state, uint32_t has_changed)
 	}
 
 	if (button & DK_BTN2_MSK) {
+		send_packet();
 	}
 }
 
@@ -238,17 +344,28 @@ int main(void)
 		LOG_ERR("Failed to initialize the LED library");
 	}
 
-	if (dk_buttons_init(button_handler) != 0) {
-		LOG_ERR("Failed to initialize the buttons library");
-	}
-
 	if (wifi_connect() != 0) {
 		LOG_ERR("Failed to connect to Wi-Fi");
 	}
 
-	while (1) {
-		k_sleep(K_FOREVER);
+	if (dk_buttons_init(button_handler) != 0) {
+		LOG_ERR("Failed to initialize the buttons library");
 	}
 
+	if (server_connect() != 0) {
+		LOG_INF("Failed to initialize client");
+		return 0;
+	}
+
+	LOG_INF("Press button 1 on your DK to enable or disable TWT");
+	/* STEP x - */
+	send_packet();
+
+	while (1) {
+		k_sleep(K_MSEC(1000));
+		/* STEP x - */
+		receive_packet();
+	}
+	close(sock);
 	return 0;
 }
