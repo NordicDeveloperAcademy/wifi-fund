0b7bb61b56955585377afdd15f23eabd34f06426
Heidi Sollie <heidisollie@gmail.com>
2025-03-17 12:30:27 +0100

    Removed redundant Kconfigs and header files, added auto_connect feature

--- v2.9.0-v2.8.0/l3/l3_e1/src/main.c
+++ v2.9.0-v2.8.0/l3/l3_e1/src/main.c
@@ -14,10 +14,8 @@
 
 #include <dk_buttons_and_leds.h>
 
-#include <zephyr/net/wifi.h>
-#include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/net/net_mgmt.h>
-#include <net/wifi_mgmt_ext.h>
+#include <zephyr/net/wifi_mgmt.h>
 #include <net/wifi_credentials.h>
 
 /* STEP 2 - Include the header file for the socket API */ 
